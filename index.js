b8574ff9bd5e7c8e332da911eda4c6790ececc04
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2020-12-07 01:15:32 +0300

    use @cloudnative/health-connect

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const app = express();
 const jose = require('node-jose');
 const fs = require('fs');
 
+const health = require('@cloudnative/health-connect');
+let healthcheck = new health.HealthChecker();
+
+const shutdownPromise = () => new Promise(function (resolve, _reject) {
+    setTimeout(function () {
+      console.log('DONE!');
+      resolve();
+    }, 10);
+  });
+let shutdownCheck = new health.ShutdownCheck("shutdownCheck", shutdownPromise);
+
+healthcheck.registerShutdownCheck(shutdownCheck);
+app.use('/live', health.LivenessEndpoint(healthcheck));
+app.use('/ready', health.ReadinessEndpoint(healthcheck));
+
 
 (async () => {
     const ks = fs.readFileSync('config/secret.json');
